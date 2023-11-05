3e27c7d44b4d4b9fd68f4c686d425d6bda07ff0d
Preout69 <100396023+Preout69@users.noreply.github.com>
2023-11-05 23:32:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,15 @@
 
 apt update
 apt upgrade -y
-apt install git
+
+apt-get install -y ca-certificates curl gnupg git
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install nodejs -y
+
 
 # Ask the user for their name
 echo Enter Git repository with Token
