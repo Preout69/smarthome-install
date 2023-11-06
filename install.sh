27d9f53518cc33e5f382d4a1dd152ff1cb5b7dec
Preout69 <100396023+Preout69@users.noreply.github.com>
2023-11-06 04:29:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 rm -R /root/smarthome/
 # Ask the user for their name
+
+echo Enter provided email token
+read emailtoken
+
+git config user.email $emailtoken
+
 echo Enter Git repository with Token
 read varname
 $varname smarthome
