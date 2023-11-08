2d77949bf58f06007482e25b5936947c0700e187
Preout69 <100396023+Preout69@users.noreply.github.com>
2023-11-08 08:42:03 +0100

    Update install-template.sh

--- install-template.sh
+++ install-template.sh
@@ -15,7 +15,7 @@ npm install pm2@latest -g
 
 #write out current crontab
 crontab -l > mycron
-echo "@reboot sh -c 'cd /root/smarthome/.output/server && pm2 start index.mjs'" >> mycron
+#echo "@reboot sh -c 'cd /root/smarthome/.output/server && pm2 start index.mjs'" >> mycron
 echo "0 3 * * *  sh -c 'cd /root/smarthome && bash update-script.sh'" >> mycron
 #install new cron file
 crontab mycron
